7f910343bb50989d4148337f01468330408aa1ff
Sunny279909 <sunyy02799@gmail.com>
2026-05-13 21:47:59 -0400

    Disable redis safely

--- server/config/redis.js
+++ server/config/redis.js
@@ -2,25 +2,27 @@ const { createClient } = require("redis");
 
 let redisClient = null;
 
-try {
-  if (process.env.REDIS_URL) {
-    redisClient = createClient({
-      url: process.env.REDIS_URL,
-    });
+if (process.env.REDIS_URL) {
+  (async () => {
+    try {
+      redisClient = createClient({
+        url: process.env.REDIS_URL,
+      });
 
-    redisClient.on("error", (err) => {
-      console.error("Redis Error:", err.message);
-    });
+      redisClient.on("error", (err) => {
+        console.log("Redis Error:", err.message);
+      });
 
-    (async () => {
       await redisClient.connect();
+
       console.log("Connected to Redis");
-    })();
-  } else {
-    console.log("Redis disabled");
-  }
-} catch (error) {
-  console.log("Redis unavailable");
+    } catch (error) {
+      console.log("Redis unavailable");
+      redisClient = null;
+    }
+  })();
+} else {
+  console.log("Redis disabled");
 }
 
 module.exports = redisClient;
